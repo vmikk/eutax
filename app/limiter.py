b8145f2164af577c0ce0f3fa2bcdac8dfc93899a
Vladimir Mikryukov <vmikryukov@gmail.com>
2025-05-08 14:53:35 +0300

    limiter - add remaining counter to responce headers

--- app/limiter.py
+++ app/limiter.py
@@ -11,4 +11,4 @@ _config = load_config()
 rate_limits = _config.get('rate_limits', {})
 
 # Initialize a Limiter instance for slowapi
-limiter = Limiter(key_func=get_remote_address)
+limiter = Limiter(key_func=get_remote_address, headers_enabled=True)
